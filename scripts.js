2e63132b04c90887935b33369e7a85a0cbc6da1d
Marcin Kozubek <mkozubek@protonmail.com>
2019-11-27 00:36:17 +0100

    Dynamically adding new recipes

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ var recipes = new Array();
 
 
 pushInitialRecipes = function() {
-    for(var i = 0; i < 3; i++) {
+    for(var i = 0; i < 2; i++) {
         recipes.push({
             shortDescription: lorem + '..<span class="link">Więcej</span>',
             description: lorem,
@@ -16,7 +16,6 @@ pushInitialRecipes = function() {
             steps: ["Marchew", "Marchew", "Marchew"]
         });
     }
-    console.log(recipes[2].ingridients);
 }
 pushInitialRecipes();
 
@@ -71,6 +70,142 @@ var expandRecipe = function(e) {
     cardDescription.innerHTML = generateExpandedDescription(recipeId);
 };
 
-Array.from(recipeCards).forEach(function(element) {
-    element.addEventListener('click', expandRecipe);
-});
+var registerRecipesForExpansion = function () {
+    Array.from(recipeCards).forEach(function(element) {
+        element.addEventListener('click', expandRecipe);
+    });
+};
+registerRecipesForExpansion();
+
+var addIngridientInput = function() {
+    var ingirdients = document.getElementById("ingridientsInput");
+
+    var li = document.createElement("li");
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("ingridientInput");
+    input.classList.add("dynamically-added");
+    input.onchange = addIngridientInput;
+
+    li.appendChild(input);
+    ingirdients.appendChild(li);
+    input.focus();
+};
+
+var addStepInput = function() {
+    var ingirdients = document.getElementById("stepsInput");
+
+    var li = document.createElement("li");
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("stepInput");
+    input.classList.add("dynamically-added");
+    input.onchange = addStepInput;
+
+    li.appendChild(input);
+    ingirdients.appendChild(li);
+    input.focus();
+};
+
+var addRecipeCard = function (id) {
+    var recipe = recipes[id];
+
+    var newCard = `<a id="recipe${id}" class="recipe" href="#">
+                        <div class="recipe-card" >
+                            <div class="flip-box">
+                                <div class="flip-box-inner">
+                                    <div class="card-image">
+                                        <img src="${recipe.imgSrc}"/>
+                                    </div>
+                                    <div class="card-image card-image-alt">
+                                        <img src="${recipe.altImgSrc}"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-description">
+                                <h3 class="recipe-title">${recipe.title}</h3>
+                                <i class="far fa-clock"></i> 45 min. <i class="far fa-user"></i> 4 os.
+                                <span onclick="deleteRecipe(${id})" style="cursor: pointer; text-decoration: underline; color: red; margin-left: 10px;">Usuń</span><br>
+                                <p>${recipe.shortDescription}</p>
+                            </div>
+                        </div>
+                    </a>`;
+
+    var recipeCardContainer = document.getElementById("recipe-card-container");
+    recipeCardContainer.innerHTML += newCard;
+
+    registerRecipesForExpansion();
+};
+
+var addRecipe = function () {
+    var title = document.getElementById("titleInput").value;
+    var description = document.getElementById("descriptionInput").value;
+    var imgSrc = document.getElementById("imgSrcInput").value;
+    var altImgSrc = document.getElementById("altImgSrcInput").value;
+
+    var ingridients = new Array();
+    let listItems = document.querySelectorAll('#ingridientsInput li input');
+    for (let i = 0; i < listItems.length; i++) {
+        if(listItems[i].value != "") {
+            ingridients.push(listItems[i].value);
+        }
+    }
+
+    var steps = new Array();
+    listItems = document.querySelectorAll('#stepsInput li input');
+    for (let i = 0; i < listItems.length; i++) {
+        if(listItems[i].value != "") {
+            steps.push(listItems[i].value);
+        }
+    }
+
+    var item = {
+        shortDescription: description.substring(0, 300) + '..<span class="link">Więcej</span>',
+        description: description,
+        ingridients: ingridients,
+        steps: steps,
+        title: title,
+        imgSrc: imgSrc,
+        altImgSrc: altImgSrc
+    }
+    
+    recipes.push(item);
+    addRecipeCard(recipes.length - 1);
+}
+
+var validateInput = function() {
+    addRecipe();
+};
+
+var clearRecipeInput = function() {
+    [].forEach.call(document.querySelectorAll('.dynamically-added'),function(e){
+        e.parentNode.parentNode.removeChild(e.parentNode);
+    });
+
+    [].forEach.call(document.querySelectorAll('#popup1 input, #popup1 textarea'),function(e){
+        e.value = "";
+    });
+};
+
+document.querySelector(".popup").addEventListener('click', e => e.stopPropagation());
+
+var closePopup = function() {
+    var url = location.href;
+    location.href = "#bestRecipes";
+    history.replaceState(null,null,url);
+};
+
+var deleteRecipe = function(id) {
+    [].forEach.call(document.querySelectorAll('.recipe'),function(recipe){
+        if(recipe.id == `recipe${id}`) {
+            recipe.parentNode.removeChild(recipe);
+        }
+    });
+
+    recipes[id] = null;
+};
+//TODO guzior z dodawaniem przepisów (użyj clearRecipeInput!)
+//scroll po naciśnięciu "Sprawdź"
+//walidacja inputu przepisu
